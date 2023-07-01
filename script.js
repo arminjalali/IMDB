a76f4a26a518d3c23970f167d4d553e9e2d861b3
arminjalali <58389402+arminjalali@users.noreply.github.com>
2023-07-01 16:37:50 +0330

    Add files via upload

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ function handleSearch() {
 
   // Make a request to search for movies
   const searchUrl = `https://imdb-api.com/en/API/SearchMovie/${apiKey}/${searchTerm}`;
-  console.log(searchUrl);
   fetchData(searchUrl)
     .then(data => {
       if (data.results.length > 0) {
@@ -26,24 +25,34 @@ function handleSearch() {
           .then(movieData => {
             // Populate the content rectangles with the movie information
             const searchedMovieElement = document.querySelector('#searched-movie');
-            const movieDetailsElement = document.querySelector('#movie-details');
 
             // Update the HTML content with the movie information
-            searchedMovieElement.innerHTML = `
-              <img src="${movieData.image}" alt="${movieData.title}">
-              <div>
-                <h2>${movieData.title}</h2>
-                <p>${movieData.description}</p>
-              </div>
-            `;
+            document.getElementById('movie-poster').src = movieData.image;
+            document.getElementById('movie-title').textContent = movieData.title;
+            document.getElementById('movie-year').textContent = movieData.year;
+            document.getElementById('movie-duration').textContent = movieData.runtimeStr;
+            document.getElementById('movie-genre').textContent = movieData.genres;
+            document.getElementById('movie-actors').textContent = movieData.stars;
+            document.getElementById('movie-rating').textContent = movieData.imDbRating;
 
-            movieDetailsElement.innerHTML = `
-              <img src="${movieData.image}" alt="${movieData.title}">
-              <div>
-                <h2>${movieData.title}</h2>
-                <p>${movieData.description}</p>
-              </div>
-            `;
+            if (movieData.ranking && movieData.ranking.rank) {
+              document.getElementById('movie-rank').textContent = movieData.ranking.rank;
+              document.getElementById('movie-awards').textContent = movieData.awards || 'None';
+            } else {
+              document.getElementById('movie-rank').textContent = 'N/A';
+              document.getElementById('movie-awards').textContent = 'None';
+            }
+
+            // Add event listener to movie genre span for hover effect
+            const genreSpan = document.getElementById('movie-genre');
+            genreSpan.addEventListener('mouseover', () => {
+              genreSpan.style.textDecoration = 'underline';
+            });
+            genreSpan.addEventListener('mouseout', () => {
+              genreSpan.style.textDecoration = 'none';
+            });
+
+            searchedMovieElement.style.display = 'block';
           });
       } else {
         // Handle case when no results are found
@@ -51,3 +60,26 @@ function handleSearch() {
       }
     });
 }
+
+// Function to make an HTTP GET request and fetch data
+function fetchData(url) {
+  return fetch(url)
+    .then(response => response.json())
+    .catch(error => console.log('Error:', error));
+}
+
+ // Add event listener to the theme toggle switch
+ const themeToggle = document.getElementById('theme-toggle');
+ themeToggle.addEventListener('change', handleThemeToggle);
+
+ // Function to handle theme toggle
+ function handleThemeToggle() {
+   const body = document.body;
+   if (themeToggle.checked) {
+     body.classList.add('dark');
+     body.classList.remove('light');
+   } else {
+     body.classList.add('light');
+     body.classList.remove('dark');
+   }
+ }
